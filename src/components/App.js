883b71d72591dd237d94b6a7ebe24aa4945e1cca
Shaikh Irfan <shaikhji1998@gmail.com>
2022-09-24 16:25:53 +0530

    Updated

--- src/components/App.js
+++ src/components/App.js
@@ -23,7 +23,7 @@ const App = () => {
                       <li>Tutorials by industry experts</li>
                       <li>Peer & expert code review</li>
                       <li>Coding exercises</li>
-                      <li>Access yo our Github repos</li>
+                      <li>Access yo our GitHub repos</li>
                       <li>Community forum</li>
                       <li>Flashcard decks</li>
                     </ul>
