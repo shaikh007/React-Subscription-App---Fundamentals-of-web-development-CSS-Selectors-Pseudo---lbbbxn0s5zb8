5b167e452c6489cfd458fd73b5c24d61b5966cdd
Shaikh Irfan <shaikhji1998@gmail.com>
2022-11-07 21:28:57 +0530

    New commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,34 +3,50 @@ import "../styles/App.css";
 const App = () => {
   return (
     <div id="main">
-      <div class="container">
+            <section class="card-section">
+              <div class="container">
                 <div class="community">
                   <h2 class="community-header">Join our community</h2>
-                  <p class="community-title-meta">30-day, hassle-free monkey back gurarantee</p>
-                  <p class="community-desciption">Gain access to our full library of tutorials along with experts code reviews. Perfect for any developers who are serious about honing thier skills.
+                  <h3 class="community-sub-header">
+                    30-day, hassle-free monkey back gurarantee
+                  </h3>
+                  <p class="community-text">
+                    Gain access to our full library of tutorials along with expert
+                    code reviews. Perfect for any developers who are serious about
+                    honing their skills.
                   </p>
                 </div>
                 <div class="card-details">
                   <div class="card">
                     <p class="card-header">Monthly Subscription</p>
-                    <span><strong class="price">$29</strong> per month</span>
-                    <span>Full access for less than $1 a day.</span>
-                    <button type="button" class="sign-up-btn">Sign Up</button>
+                    <div class="price">
+                      <span>$29</span>
+                      <p>Per Month</p>
+                    </div>
+                    <p class="card-details-text">
+                      Full access for less than $1 a day.
+                    </p>
+                    <a href="#" class="btn btn-card">
+                      Sign Up
+                    </a>
                   </div>
                   <div class="why-us">
                     <h4 class="why-us-header">Why Us</h4>
-                    <ul>
-                      <li>Tutorials by industry experts</li>
-                      <li>Peer & expert code review</li>
-                      <li>Coding exercises</li>
-                      <li>Access yo our GitHub repos</li>
-                      <li>Community forum</li>
-                      <li>Flashcard decks</li>
-                    </ul>
+                    <div class="why-us-lists">
+                      <ul class="why-us-items">
+                        <li class="why-us-item">Tutorials by industry experts</li>
+                        <li class="why-us-item">Peer & expert code review</li>
+                        <li class="why-us-item">Coding exercises</li>
+                        <li class="why-us-item">Access yo our GitHub repos</li>
+                        <li class="why-us-item">Community forum</li>
+                        <li class="why-us-item">Flashcard decks</li>
+                      </ul>
+                    </div>
                   </div>
                 </div>
               </div>
-    </div>
+            </section>
+          </div> 
   );
 };
 
